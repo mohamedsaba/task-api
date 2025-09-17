28719a51309e8b4ad578947fea521e3ca3544d27
mohamedsaba <151614379+mohamedsaba@users.noreply.github.com>
2025-09-18 00:11:37 +0300

    Update users.service.ts

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,29 +1,45 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException, UnauthorizedException } from "@nestjs/common";
 import { UserEntity } from "src/database/entities/user.entity";
 import fs from 'fs'
 import path from 'path'
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import { CreateUserDto } from '../../shared/dtos/create-user.dto';
+import { UpdateUserDto } from "src/shared/dtos/update-user.dto";
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class UsersServices{
 
     constructor(@InjectRepository(UserEntity) private userRepository : Repository<UserEntity>){}
 
     async addUser(user : UserEntity){
-        const newUser = await this.userRepository.create(user);
-        return await this.userRepository.save(newUser);
+        return await this.userRepository.save(user);
     }
 
-    getMyProfile(){
-
+    async getMyProfile(id : string){
+        const user = await this.userRepository.findOne({where : {id}});
+        if(!user) throw new NotFoundException('There is no user with such id');
+        return user;
     }
 
-    updateMyProfile(){
-
+    async updateMyProfile(id : string , body : UpdateUserDto){
+        const user = await this.getMyProfile(id);
+        const {username , email } = body;
+        const isEmailFound = await this.userRepository.findOne({where : {email}});
+        if(email && isEmailFound) throw new UnauthorizedException('Email is ALready in use');
+        const isUsernameFound = await this.userRepository.findOne({where : {username}}); 
+        if(username && isUsernameFound) throw new UnauthorizedException('username is already in use');
+        const newUser = {...user , username , email};
+        await this.userRepository.save(newUser);
+        return {message : `User : ${user.id}'s profile has been updated successfully` , newUser}
     }
 
-    changePassword(){
-
+    async changePassword(id : string , password : string){
+        const user = await this.getMyProfile(id);
+        const hashedPassword = bcrypt.hashSync(password , 10);
+        const newUser = {...user , password : hashedPassword};
+        await this.userRepository.save(newUser);
+        return {message : `Password Has Been Changed Successfully`}
     }
 
     async getUserById(id : string){
